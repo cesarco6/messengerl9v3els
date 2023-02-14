82fba22f7c8cd7515d703b57e883365d2415eef9
Cesar <alfredocesarco@gmail.com>
2023-02-13 23:00:27 -0600

    inicio del proyecto laravel con vue, Element+ y sass configurados

---
+++ resources/js/Page/app.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ElButton } from 'element-plus'
+
+
+</script>
+<template>
+    <h1>
+        Laravel with vue 3 Sass and Element plush with Vite
+    </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nulla aliquet porttitor lacus luctus. Egestas dui id ornare arcu odio ut. Enim neque volutpat ac tincidunt vitae semper. Faucibus turpis in eu mi. Tortor at risus viverra adipiscing at. Eget felis eget nunc lobortis mattis. Sit amet nisl suscipit adipiscing bibendum. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Donec et odio pellentesque diam volutpat commodo. Interdum velit laoreet id donec. Pellentesque nec nam aliquam sem et tortor consequat id.</p>
+    
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+//import './bootstrap';
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+
+import App from './Page/app.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
+
+//createApp(app).mount("#app")
